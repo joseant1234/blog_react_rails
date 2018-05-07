695c2b63151b15324f5720d9e6be8cf98c4a14ab
Jose Tafur <j-osea-nt@hotmail.com>
2018-05-06 19:27:23 -0500

    add webpacker-react, login form

--- Gemfile
+++ Gemfile
@@ -29,6 +29,7 @@ gem 'jbuilder', '~> 2.5'
 # Use ActiveModel has_secure_password
 # gem 'bcrypt', '~> 3.1.7'
 
+# check if version of webpacker-react is same of ./bin/yarn add webpacker-react
 gem 'webpacker-react'
 
 # Use ActiveStorage variant
--- app/javascript/components/registration/login.jsx
+++ app/javascript/components/registration/login.jsx
@@ -5,18 +5,55 @@ import TextField from 'material-ui/TextField';
 import RaisedButton from 'material-ui/RaisedButton';
 
 export class Login extends React.Component{
+
+	constructor(props){
+		super(props);
+		this.state = {
+			canSubmit: true,
+			email: '',
+			password: '',
+			error: ''
+		};
+	}
+
+	enableSubmitBtn(){
+		this.setState({
+			canSubmit: true
+		});
+	}
+
+	disableSubmitBtn(){
+		this.setState({
+			canSubmit: false
+		});
+	}
+
+	syncField(ev,fieldName){
+		let element = ev.target;
+		let value = element.value;
+
+		let jsonState = {}
+		jsonState[fieldName] = value;
+		this.setState(jsonState)
+	}
+
+
+	// cuando el state se actualiza, react llama al metodo render para evaluar q parte se modifico y se debe de actualizar en la vista
 	render(){
 		return(
 			<MuiThemeProvider>
 				<form>
+					<p>{this.state.email}</p>
 					<div>
 						<TextField
+							onChange={(e)=> this.syncField(e,"email")}
 							name="email"
 							required
 							floatingLabelText="Correo electronico"/>
 					</div>
 					<div>
 						<TextField
+							onChange={(e)=> this.syncField(e,"password")}
 							name="password"
 							required
 							type="password"
@@ -24,6 +61,7 @@ export class Login extends React.Component{
 					</div>
 					<div>
 						<RaisedButton
+							disabled={!this.state.canSubmit}
 							type="submit"
 							label="Iniciar sesion" />
 					</div>
